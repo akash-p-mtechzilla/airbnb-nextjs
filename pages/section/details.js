5941a8ec0dd07299642e054526b201f153eaa060
akash <akash.p@mtechzilla.com>
2022-10-11 11:39:43 +0530

    details page ui fixed 3

--- pages/section/details.js
+++ pages/section/details.js
@@ -114,7 +114,7 @@ const DetailsPage = () => {
 
           <Text fontSize='1.7rem'
           >
-            Where you'll sleep
+            Where youll sleep
           </Text>
 
           <Box maxW="7xl" mx={'auto'} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
